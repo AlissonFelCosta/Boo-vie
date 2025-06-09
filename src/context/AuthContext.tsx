c857dda51e083f80bc98c4665e2536adf6a644d5
Alisson Felipe <138620602+AlissonFelCosta@users.noreply.github.com>
2025-06-09 16:48:52 -0300

    ✨Update

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -124,7 +124,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
         email,
         password,
         options: {
-          emailRedirectTo: window.location.origin
+          emailRedirectTo: `${window.location.origin}/email-confirmation`
         }
       });
 
